863e32edc0ddcd83f02b08431b72736a747f6c48
Matthew C <matthew.j.carpenter1@gmail.com>
2019-04-23 09:35:16 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+require('./routes/api-routes')(app)
+
+
+app.listen( PORT, () => {
+  console.log(`App now listening on ${PORT}`)
+})
