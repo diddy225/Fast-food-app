5530ce41c9f1eb71254f7dd54e5206e593a0fa28
Matthew C <matthew.j.carpenter1@gmail.com>
2019-04-28 10:43:04 -0400

    added rabbitMQ

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,17 +1,24 @@
-let arrivalTime = document.getElementById('form-arrival-time');
-let prepTime = document.getElementById('form-prep-time');
-const startButton = document.getElementById('btn');
+let arrivalTime = document.getElementById("form-arrival-time");
+let prepTime = document.getElementById("form-prep-time");
+const startButton = document.getElementById("btn");
+let customerNumber = 100;
 
-startButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  arrivalTime = arrivalTime.value
-  prepTime = prepTime.value
-});
-
-const intervalTime = (arrival) => {
-  let customerNum = 100;
-  return setInterval(() => {console.log(`Customer: ${customerNum++}`)}, arrival * 1000)
+//empty input field -> put in project lib js file.
+const empty = (element) => {
+  element.value = ""
 }
 
+//Initiates the customer arrival time and prep time
+startButton.addEventListener("click", e => {
+  e.preventDefault();
+  
+  if (isNaN(arrivalTime.value) || arrivalTime.value === "") {
+    empty(arrivalTime)
+    empty(prepTime)
+    return alert(`You entered ${arrivalTime.value} and ${prepTime.value}, these should be numbers.`);
+  }
 
-
+  return setInterval(() => {
+    console.log(`Customer: ${customerNumber++}`);
+  }, arrivalTime.value * 1000);
+});
